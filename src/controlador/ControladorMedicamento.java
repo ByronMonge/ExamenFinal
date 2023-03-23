91af0575b05587fc719a84b2450e43f552ceeadb
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-23 12:20:11 -0500

    Proyecto t

--- src/controlador/ControladorMedicamento.java
+++ src/controlador/ControladorMedicamento.java
@@ -60,7 +60,7 @@ public class ControladorMedicamento {
 
         ConectPG cpg = new ConectPG();//Instanciar la conexion con esto abrimos la conexion a la BD
         try {
-            JasperReport jr = (JasperReport) JRLoader.loadObject(getClass().getResource("/reportes/CursoReporte.jasper"));
+            JasperReport jr = (JasperReport) JRLoader.loadObject(getClass().getResource("/reporte/CursoReporte.jasper"));
 
             //Hacer una vista previa
             //JasperPrint jp = JasperFillManager.fillReport(jr, null, cpg.getCon());//JasperFillManager.fillReport: Carga los datos de la BD.//JasperPrint: Hace la impresion del reporte. Puede ir 'null' si en el jasper no existen parametros caso contrario se envian los parametros necesarios
